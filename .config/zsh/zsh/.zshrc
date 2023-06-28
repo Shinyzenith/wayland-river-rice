Shinyzenith/.dotfiles

[push] .dotfiles
Signed-off-by: Shinyzenith <aakashsensharma@gmail.com>

.config/zsh/zsh/.zshrc
@@ -1,0 +1,95 @@
+# ____  _     _                           _ _   _         _____    _
+#/ ___|| |__ (_)_ __  _   _ _______ _ __ (_) |_| |__     |__  /___| |__  _ __ ___
+#\___ \| '_ \| | '_ \| | | |_  / _ \ '_ \| | __| '_ \      / // __| '_ \| '__/ __|
+# ___) | | | | | | | | |_| |/ /  __/ | | | | |_| | | |  _ / /_\__ \ | | | | | (__
+#|____/|_| |_|_|_| |_|\__, /___\___|_| |_|_|\__|_| |_| (_)____|___/_| |_|_|  \___|
+#                     |___/
+
+export PROMPT="%F{078}%~"$'\n'" ❯ %f"
+autoload -U promptinit; promptinit
+autoload -U colors && colors
+export PWD=/home/$USER
+export OLDPWD=/home/$USER
+export BAT_THEME="Nord"
+
+export KEYTIMEOUT=1
+export ZSH=/usr/share/oh-my-zsh
+source $ZSH/oh-my-zsh.sh
+export EDITOR=/usr/bin/nvim
+export VISUAL=/usr/bin/nvim
+export _JAVA_AWT_WM_NONREPARENTING=1
+export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true"
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+
+HISTSIZE=50000
+SAVEHIST=50000
+HISTFILE=~/.config/zsh/zsh_history
+HIST_STAMPS="dd/mm/yyyy"
+compinit
+eval "$(zoxide init zsh)"
+_comp_options+=(globdots) # lets you tab complete hidden files by default
+
+#(cat ~/.cache/wal/sequences &)
+plugins=(
+	zsh-autosuggestions
+	vi-mode
+	zsh-syntax-highlighting
+	)
+
+# Change cursor shape for different vi modes.
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] ||
+	 [[ $1 = 'block' ]]; then
+	echo -ne '\e[1 q'
+  elif [[ ${KEYMAP} == main ]] ||
+	   [[ ${KEYMAP} == viins ]] ||
+	   [[ ${KEYMAP} = '' ]] ||
+	   [[ $1 = 'beam' ]]; then
+	echo -ne '\e[5 q'
+  fi
+}
+zle -N zle-keymap-select
+zle-line-init() {
+	zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+	echo -ne "\e[5 q"
+}
+zle -N zle-line-init
+echo -ne '\e[5 q' # Use beam shape cursor on startup.
+preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+
+# ci"
+autoload -U select-quoted
+zle -N select-quoted
+for m in visual viopp; do
+  for c in {a,i}{\',\",\`}; do
+	bindkey -M $m $c select-quoted
+  done
+done
+
+# ci{, ci(, di{ etc..
+autoload -U select-bracketed
+zle -N select-bracketed
+for m in visual viopp; do
+  for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+	bindkey -M $m $c select-bracketed
+  done
+done
+
+# Use vim keys in tab complete menu:
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -v '^?' backward-delete-char
+bindkey '^R' history-incremental-search-backward
+bindkey '^E' edit-command-line
+bindkey -v 
+
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+[[ -f $ZDOTDIR/.zshrc-alias ]] && . $ZDOTDIR/.zshrc-alias
+[[ -f $ZDOTDIR/fzf/completion.zsh ]] && . $ZDOTDIR/fzf/completion.zsh
+[[ -f $ZDOTDIR/fzf/key-bindings.zsh ]] && . $ZDOTDIR/fzf/key-bindings.zsh
+[[ ! -f ${XDG_CACHE_HOME:-${HOME}/.cache}/neofetch-cache ]] && /usr/bin/neofetch > ~/.cache/neofetch-cache
+=cat ${XDG_CACHE_HOME:-${HOME}/.cache}/neofetch-cache
+precmd(){print""}

setup.sh
@@ -1,0 +1,101 @@
+#!/usr/bin/env bash
+# installing our package manager
+doas pacman -S --noconfirm --needed git
+if ! command -v paru &> /dev/null
+then
+	git clone https://aur.archlinux.org/paru-bin.git /tmp/paru-bin-cloned
+	cd /tmp/paru-bin-cloned/
+	makepkg -sfci --noconfirm --needed
+fi
+# arch linux support
+doas pacman -S --noconfirm --needed artix-archlinux-support
+doas pacman-key --init
+doas pacman-key --populate artix
+doas pacman-key --populate archlinux
+
+cd ~/.config/.dotfiles
+doas cp ./assets/pacman.conf /etc/pacman.conf
+doas pacman -Syy
+
+
+# makepkg.conf optimization
+numberofcores=$(grep -c ^processor /proc/cpuinfo)
+if [ $numberofcores -gt 1 ]
+then
+        doas sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j'$(($numberofcores+1))'"/g' /etc/makepkg.conf;
+        doas sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -z - --threads=0)/g' /etc/makepkg.conf
+        doas sed -i 's/COMPRESSZST=(zstd -c -z -q -)/COMPRESSZST=(zstd -c -z -q - --threads=0)/g' /etc/makepkg.conf
+        doas sed -i "s/PKGEXT='.pkg.tar.xz'/PKGEXT='.pkg.tar.zst'/g" /etc/makepkg.conf
+else
+        echo "makepkg.conf not changed."
+fi
+
+
+# Doom emacs.
+# paru -S emacs-gcc-wayland-devel-bin --noconfirm
+# export DOOMDIR="${XDG_CONFIG_HOME:-$HOME/.config}/doom"
+# git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
+# ~/.config/emacs/bin/doom install
+# rm -rf ~/.config/doom
+
+for line in $(cat ./assets/packagelist)
+do
+	doas pacman -S --noconfirm --needed $line
+done
+
+for line in $(cat ./assets/aurlist)
+do
+	if [[ $(pacman -Qs $line) == "" ]]
+	then
+		paru -S --noconfirm --needed $line
+	fi
+done
+
+#cleaning up orphans
+doas pacman -Rns --noconfirm scdoc
+doas pacman -Rns --noconfirm python-pytest
+doas pacman -Rdd --noconfirm xorg-server
+doas pacman -Rdd --noconfirm foot-themes
+doas pacman -S --needed wireplumber
+doas pacman -S --needed pipewire-pulse
+paru -c --noconfirm
+paru --gendb
+
+doas ln -s /etc/runit/sv/bluetoothd /run/runit/service
+doas ln -s /etc/runit/sv/sshd /run/runit/service
+
+xdg-user-dirs-update &
+cd ~/.config/.dotfiles
+doas mkdir /usr/share/btop/themes
+doas cp ./assets/tokyo.theme /usr/share/btop/themes/tokyo.theme
+./config.sh
+~/.config/emacs/bin/doom sync
+doas ln -s ~/.config/swhkd/swhkdrc /etc/swhkd/swhkdrc
+doas python3 -m pip install neovim
+doas python3 -m pip install beautifulsoup4
+doas npm install neovim --global
+doas npm install prettier --global
+mkdir -p ~/Pictures/screenshots
+mkdir -p ~/Videos/recordings
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim -c :PlugInstall -c :exit -c :exit
+doas mkdir -p /usr/share/icons/default
+doas touch /usr/share/icons/default/index.theme
+doas chown $USER /usr/share/icons/default/index.theme
+doas chown $USER /usr/share/backgrounds
+doas echo "[Icon Theme]" > /usr/share/icons/default/index.theme
+doas echo "Inherits=macOSBigSur" >> /usr/share/icons/default/index.theme
+doas chown root /usr/share/icons/default/index.theme
+doas gpasswd -a $USER video
+doas gpasswd -a $USER power
+doas gpasswd -a $USER audio
+rm -rf .bash_history
+rm -rf .bash_logout
+rm -rf .bashrc
+rm -rf .viminfo
+rm -rf ~/.npm
+rm -rf ~/Templates
+rm -rf ~/Public
+xdg-user-dirs-update
+chsh --shell $(which zsh) $USER

Shinyzenith/wayland-river-rice

.config/zsh/zsh/.zshrc
@@ -1,0 +1,101 @@
+# ____  _     _                           _ _   _         _____    _
+#/ ___|| |__ (_)_ __  _   _ _______ _ __ (_) |_| |__     |__  /___| |__  _ __ ___
+#\___ \| '_ \| | '_ \| | | |_  / _ \ '_ \| | __| '_ \      / // __| '_ \| '__/ __|
+# ___) | | | | | | | | |_| |/ /  __/ | | | | |_| | | |  _ / /_\__ \ | | | | | (__
+#|____/|_| |_|_|_| |_|\__, /___\___|_| |_|_|\__|_| |_| (_)____|___/_| |_|_|  \___|
+#                     |___/
+
+#export PROMPT="%F{078}%~"$'\n'" ❯ %f"
+eval "$(starship init zsh)"
+autoload -U promptinit; promptinit
+autoload -U colors && colors
+export PWD=/home/$USER
+export OLDPWD=/home/$USER
+export BAT_THEME="base16"
+
+export KEYTIMEOUT=1
+export ZSH=/usr/share/oh-my-zsh
+source $ZSH/oh-my-zsh.sh
+export EDITOR=/usr/bin/nvim
+export VISUAL=/usr/bin/nvim
+export _JAVA_AWT_WM_NONREPARENTING=1
+export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true"
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+export DIRENV_LOG_FORMAT=
+
+HISTSIZE=50000
+SAVEHIST=50000
+HISTFILE=~/.config/zsh/zsh_history
+HIST_STAMPS="dd/mm/yyyy"
+compinit
+eval "$(zoxide init zsh)"
+_comp_options+=(globdots) # lets you tab complete hidden files by default
+
+#(cat ~/.cache/wal/sequences &)
+plugins=(
+	zsh-autosuggestions
+	vi-mode
+	zsh-syntax-highlighting
+	)
+
+# Change cursor shape for different vi modes.
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] ||
+	 [[ $1 = 'block' ]]; then
+	echo -ne '\e[1 q'
+  elif [[ ${KEYMAP} == main ]] ||
+	   [[ ${KEYMAP} == viins ]] ||
+	   [[ ${KEYMAP} = '' ]] ||
+	   [[ $1 = 'beam' ]]; then
+	echo -ne '\e[5 q'
+  fi
+}
+zle -N zle-keymap-select
+zle-line-init() {
+	zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+	echo -ne "\e[5 q"
+}
+zle -N zle-line-init
+echo -ne '\e[5 q' # Use beam shape cursor on startup.
+preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+
+# ci"
+autoload -U select-quoted
+zle -N select-quoted
+for m in visual viopp; do
+  for c in {a,i}{\',\",\`}; do
+	bindkey -M $m $c select-quoted
+  done
+done
+
+# ci{, ci(, di{ etc..
+autoload -U select-bracketed
+zle -N select-bracketed
+for m in visual viopp; do
+  for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+	bindkey -M $m $c select-bracketed
+  done
+done
+
+# Use vim keys in tab complete menu:
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -v '^?' backward-delete-char
+bindkey '^R' history-incremental-search-backward
+bindkey '^E' edit-command-line
+bindkey -v 
+
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+[[ -f $ZDOTDIR/.zshrc-alias ]] && . $ZDOTDIR/.zshrc-alias
+[[ -f $ZDOTDIR/fzf/completion.zsh ]] && . $ZDOTDIR/fzf/completion.zsh
+[[ -f $ZDOTDIR/fzf/key-bindings.zsh ]] && . $ZDOTDIR/fzf/key-bindings.zsh
+[[ ! -f ${XDG_CACHE_HOME:-${HOME}/.cache}/neofetch-cache ]] && /usr/bin/neofetch > ~/.cache/neofetch-cache
+=cat ${XDG_CACHE_HOME:-${HOME}/.cache}/neofetch-cache
+precmd(){print""}
+
+# Nix stuff
+. /home/aakash/.local/state/nix/profile/etc/profile.d/nix.sh
+eval "$(direnv hook zsh)"

setup.sh
@@ -1,0 +1,103 @@
+#!/usr/bin/env bash
+# installing our package manager
+doas pacman -S --noconfirm --needed git
+if ! command -v paru &> /dev/null
+then
+	git clone https://aur.archlinux.org/paru-bin.git /tmp/paru-bin-cloned
+	cd /tmp/paru-bin-cloned/
+	makepkg -sfci --noconfirm --needed
+fi
+
+cd ~/.config/.dotfiles
+doas cp ./assets/pacman.conf /etc/pacman.conf
+doas pacman -Syy
+
+# arch linux support
+doas pacman -S --noconfirm --needed artix-archlinux-support
+doas pacman-key --init
+doas pacman-key --populate artix
+doas pacman-key --populate archlinux
+
+
+# makepkg.conf optimization
+numberofcores=$(grep -c ^processor /proc/cpuinfo)
+if [ $numberofcores -gt 1 ]
+then
+        doas sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j'$(($numberofcores+1))'"/g' /etc/makepkg.conf;
+        doas sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -z - --threads=0)/g' /etc/makepkg.conf
+        doas sed -i 's/COMPRESSZST=(zstd -c -z -q -)/COMPRESSZST=(zstd -c -z -q - --threads=0)/g' /etc/makepkg.conf
+        doas sed -i "s/PKGEXT='.pkg.tar.xz'/PKGEXT='.pkg.tar.zst'/g" /etc/makepkg.conf
+else
+        echo "makepkg.conf not changed."
+fi
+
+
+# Doom emacs.
+# paru -S emacs-gcc-wayland-devel-bin --noconfirm
+# export DOOMDIR="${XDG_CONFIG_HOME:-$HOME/.config}/doom"
+# git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
+# ~/.config/emacs/bin/doom install
+# rm -rf ~/.config/doom
+# ~/.config/emacs/bin/doom sync
+
+for line in $(cat ./assets/packagelist)
+do
+	doas pacman -S --noconfirm --needed $line
+done
+
+for line in $(cat ./assets/aurlist)
+do
+	if [[ $(pacman -Qs $line) == "" ]]
+	then
+		paru -S --noconfirm --needed $line
+	fi
+done
+
+#cleaning up orphans
+doas pacman -Rns --noconfirm sudo
+doas pacman -Rns --noconfirm scdoc
+doas pacman -Rdd --noconfirm xorg-server
+doas pacman -Rdd --noconfirm foot-themes
+doas pacman -Rns --noconfirm python-pytest
+doas pacman -S --needed wireplumber
+doas pacman -S --needed pipewire-pulse
+paru -c --noconfirm
+paru --gendb
+
+doas ln -s /etc/runit/sv/bluetoothd /run/runit/service
+doas ln -s /etc/runit/sv/sshd /run/runit/service
+
+xdg-user-dirs-update &
+cd ~/.config/.dotfiles
+doas mkdir /usr/share/btop/themes
+doas cp ./assets/tokyo.theme /usr/share/btop/themes/tokyo.theme
+./config.sh
+doas ln -s ~/.config/swhkd/swhkdrc /etc/swhkd/swhkdrc
+doas python3 -m pip install neovim
+doas python3 -m pip install beautifulsoup4
+doas npm install neovim --global
+doas npm install prettier --global
+mkdir -p ~/Pictures/screenshots
+mkdir -p ~/Videos/recordings
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+nvim -c :PackerSync -c :exit -c :exit
+doas mkdir -p /usr/share/icons/default
+doas touch /usr/share/icons/default/index.theme
+doas chown $USER /usr/share/icons/default/index.theme
+doas chown $USER /usr/share/backgrounds
+doas echo "[Icon Theme]" > /usr/share/icons/default/index.theme
+doas echo "Inherits=macOSBigSur" >> /usr/share/icons/default/index.theme
+doas chown root /usr/share/icons/default/index.theme
+doas gpasswd -a $USER video
+doas gpasswd -a $USER power
+doas gpasswd -a $USER audio
+rm -rf .bash_history
+rm -rf .bash_logout
+rm -rf .bashrc
+rm -rf .viminfo
+rm -rf ~/.npm
+rm -rf ~/Templates
+rm -rf ~/Public
+xdg-user-dirs-update
+chsh --shell $(which zsh) $USER